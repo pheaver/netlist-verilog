830c4ff0993345aa9c13bb520cb66d88cdb408f6
Philip Weaver <pweaver@signalicorp.com>
2010-06-21 04:54:43 -0700

    fix top-level Verilog module

--- verilog/Language/Verilog.hs
+++ verilog/Language/Verilog.hs
@@ -2,11 +2,11 @@
 -- Copyright (c) 2010 Signali Corp.
 -- -----------------------------------------------------------------------------
 
-module Language.Verilog ( module Language.Verilog.AST
+module Language.Verilog ( module Language.Verilog.Syntax
                         , module Language.Verilog.PrettyPrint
                         ) where
 
-import Language.Verilog.AST
+import Language.Verilog.Syntax
 import Language.Verilog.PrettyPrint
 
 -- -----------------------------------------------------------------------------
